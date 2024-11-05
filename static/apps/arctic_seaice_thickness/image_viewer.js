6f7564fa02e27f0040e0089e2b80c3e270fe253a
Ben Palmer <b.c.palmer@northumbria.ac.uk>
2024-11-05 14:48:26 +0000

    Added range slider to select time period

--- static/apps/arctic_seaice_thickness/image_viewer.js
+++ static/apps/arctic_seaice_thickness/image_viewer.js
@@ -1,40 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
     const imageSelect = document.getElementById('imageSelect');
+    const displayArea = document.getElementById('displayArea');
     const displayImage = document.getElementById('displayImage');
+    const displayText = document.getElementById('displayText');
+    let images;
 
     // Function to fetch the list of image files from the JSON file
     async function fetchImageList() {
         try {
             const response = await fetch('data/image_list.json'); // Path to your JSON file
-            const images = await response.json();
-            populateSelectBox(images);
+            images = await response.json();
+            populateSlider(images);
         } catch (error) {
             console.error('Error fetching image list:', error);
         }
     }
+    
+    function populateSlider(images) {
+        imageSelect.max = images.length - 1;
+        imageSelect.value = images.length - 1;
 
-    // Function to populate the select box with image options
-    function populateSelectBox(images) {
-        images.reverse().forEach((image, key, arr) => {
-            const option = document.createElement('option');
-            option.value = image.fileName;
-            option.textContent = image.displayName;
-            imageSelect.appendChild(option);
+        // Add event listener to the slider
+        imageSelect.addEventListener('input', () => {
+            const selectedIndex = parseInt(imageSelect.value);
+            if (images[selectedIndex]) {
+                displayImage.src = `data/images/${images[selectedIndex].fileName}`; // Adjust the path to your images directory if needed
+                displayArea.hidden = false;
+                displayText.innerHTML = images[selectedIndex].displayName
+            } else {
+                displayImage.src = '';
+                displayArea.hidden = true;
+            }
         });
     }
 
-    // Event listener to handle image selection
-    imageSelect.addEventListener('change', function() {
-        const selectedImage = imageSelect.value;
-        if (selectedImage) {
-            displayImage.src = `data/images/${selectedImage}`; // Adjust the path to your images directory if needed
-            displayImage.hidden = false;
-        } else {
-            displayImage.src = '';
-            displayImage.hidden = true;
-        }
-    });
-
-    // Fetch the image list on page load
-    fetchImageList();
+    fetchImageList()
 });
